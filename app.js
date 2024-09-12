bce13d26038c153d52a93737b1b21550b2f55ebd
mtyd2008 <tahaydandya@gmail.com>
2024-09-12 15:21:23 +0500

    3 chapters done

---
+++ app.js
@@ -0,0 +1,135 @@
+// CHAPTER 1
+
+// answer 1
+//  alert("Welcome to our Website");
+
+// answer 2
+// alert ("Error! Please enter a valid password.")
+
+// ansnser 3
+// alert("Welcome to JS Land... \nHappy Coding!")  
+
+// answer 4
+// alert("Welcome to JS Land...")
+// alert("Happy Coding! \nPrevent this page from creating additional dialogs")
+
+// answer 5
+// alert("Hello... I can run JS through my web browser's console")
+
+// CHAPTER 2
+
+// answer 1
+// var username = "Taha"
+
+// answer 2
+// var myName = "Taha"
+
+// answer 3
+// var message = "Hello world"
+// alert (message)
+
+
+// answer 4
+// var studentname = "Jhone Doe"
+// var studentage = 15
+// var studentacheive = "Certified Mobile Applications Development"
+
+// alert(studentname);
+// alert(studentage);
+// alert(studentacheive);
+
+
+// answer 5
+// var pizza = "PIZZA \nPIZZ \nPIZ \nPI \nP"
+// alert(pizza)
+
+// answer 6
+// var email = "tahaydandya@gmail.com"
+// alert("my email address is"  + email)
+
+// answer 7
+// var book = "A smarter way to learn JavaScript"
+// alert("I am trying to learn from the Book" + book)
+
+// answer 8
+// console.log("Yah! I can write HTML content through JavaScript");
+
+// CHAPTER 3 
+
+// answer 1
+// var age = "I am 15 years old"
+// alert(age)
+
+// answer 2
+// var tracker = "You have visited this site 14 times"
+// alert(tracker)
+
+
+// answer 3
+// var birthyear = 2008
+// console.log("My birth year is" + birthyear)
+// console.log("Data type of my declared variable is number");
+
+
+// answer 4
+// var visitorname = "John Doe"
+// var orderquantity = "5"
+// var producttitle = "T-shirts"
+
+// console.log(visitorname + "ordered" + orderquantity + producttitle + "on XYZ Clothing store")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
