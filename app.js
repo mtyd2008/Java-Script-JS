b8e82dc38897d35ef130c66e2f45e789f78a08dc
mtyd2008 <tahaydandya@gmail.com>
2024-09-13 12:03:06 +0500

    5 chapters complete

--- app.js
+++ app.js
@@ -100,27 +100,81 @@
 // name&fathername
 // function
 
+// CHAPTER 5
+
+// answer 1
+// num1 = 23;
+// num2 = 7;
+// var sum = num1 + num2;
+// console.log("Sum of num1 and num2 is" + sum);
 
 
+// answer 2
+// var sum = num1 - num2;
+// console.log(sum);
+
+// var sum = num1 * num2;
+// console.log(sum);
+
+// var sum = num1 / num2;
+// console.log(sum);
+
+// var sum = num1 % num2;
+// console.log(sum);
 
 
+// answer 3
+// num1 = 7
+// num2 = 3
 
+// var Initialvalue = 5
 
+// var afterincrement = 6
 
+// var addition = 6 + num1
 
+// var afteraddition = 13
 
+// var afterdecrement = 12
 
+// var divideby3 = 12 / 3
 
+// var remainder = 0
 
+// console.log("Initial value:" + Initialvalue)
+// console.log("Value after increment is:" + afterincrement);
+// console.log("Value after addition is:" + afteraddition);
+// console.log("Value after decrement is:" + afterdecrement);
+// console.log("The remainder is:" + remainder);
 
+// answer 4 
+// var ticket = 600
+// console.log("Total cost to buy 5 tickets to a movie is" + ticket*5 + "PKR");
 
+// answer 5
+// var Table = "Table of 4"
+// var sum1 = "4x1 = 4"
+// var sum2 = "4x2 = 8"
+// var sum3 = "4x3 = 12"
+// var sum4 = "4x4 = 16"
+// var sum5 = "4x5 = 20"
+// var sum6 = "4x6 = 24"
+// var sum7 = "4x7 = 28"
+// var sum8 = "4x8 = 36"
+// var sum9 = "4x9 = 32"
+// var sum10 = "4x10 = 40"
 
-
-
-
-
-
-
+// console.log(Table);
+// console.log(sum1);
+// console.log(sum2);
+// console.log(sum3);
+// console.log(sum4);
+// console.log(sum5);
+// console.log(sum6);
+// console.log(sum7);
+// console.log(sum8);
+// console.log(sum9);
+// console.log(sum10);
 
 
 
